41700fe5a0cd2483c08b85667273a481e821ac4b
Marcus Lai <marcus41881653@gmail.com>
2022-02-22 14:40:24 -0800

    notification sound effect, icon, formatting all-around, quit notification-mode now possible

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,9 @@
+from re import S
 import sqlite3
 import datetime
 from notifypy import Notify
 import time
+from threading import Thread
 
 # this class handles the functions associated with the scheduler
 class Scheduler:
@@ -41,6 +43,11 @@ class Scheduler:
         WHERE task = '{taskname}'
         """)
         connection.commit()
+    
+    @staticmethod
+    def notification_mode_input():
+        x = input()
+        result[0] = x.strip()
 
 # this is the structure of the table in schedule_db.db:
 # CREATE TABLE schedule(
@@ -56,7 +63,7 @@ c = connection.cursor()
 # this part loops the input 
 while True:
     task_list = Scheduler.get_tasks()
-    print('|Task Scheduler ("help" for help)|\nCurrent tasks:')
+    print('\n|Task Scheduler - Scheduling Mode| ("help" for help)\n|Current tasks|')
     if len(task_list)!=0:
         for i in task_list:
             print(i)
@@ -64,13 +71,13 @@ while True:
     key = command.strip().split()[0]
     if command.strip() == 'help':
         print("""
-        Commands and Usage:
-                add [task_name] [task_date(yyyy-mm-dd)] [task_time(hh:mm:ss)]: adds a task
-                del [task_name]: deletes a task 
-                clear: clears all scheduled tasks
-                notification mode: enter notification mode (you won't be able to edit your schedule 
-                    but you will be notified of your tasks 30 minutes prior to each)
-                quit: quit the program
+Commands and Usage:
+    add [task_name] [task_date(yyyy-mm-dd)] [task_time(hh:mm:ss)]: adds a task
+    del [task_name]: deletes a task 
+    clear CONFIRM: clears all scheduled tasks
+    notification mode: enter notification mode (you will be notified
+    for your tasks 30 minutes prior)
+    quit: quit the program
         """)
 
     elif command.strip() == 'quit':
@@ -85,42 +92,62 @@ while True:
         task = command.strip().strip().split()[1]
         Scheduler.del_task(task)
 
-    elif command.strip() == 'clear':
+    elif command.strip() == 'clear CONFIRM':
         Scheduler.clear_tasks()
         
     elif command.strip() == 'notification mode':
-        print('You are in notification mode, once you are notified of all your tasks this mode will be exited.')
-        while True:
-            c.execute("""SELECT task,date,time,notified FROM schedule""")
-            task_list = c.fetchall()
-            now = datetime.datetime.now()
 
-            # this counter checks if all tasks have been notified
-            counter = 0
+        print('''\n|Task Scheduler - Notification Mode| ("quit" to reenter Scheduling Mode)''')
+
+        #checks if all tasks notified
+        c.execute("""SELECT task,date,time,notified FROM schedule
+        WHERE notified = 0
+        """)
+        task_list = c.fetchall()
+        if len(task_list)==0:
+            print('All tasks notified, "quit" to return...\n')
+        else:
+            print('|Unnotified tasks|')
             for i in task_list:
-                if i[3]==False:
-                    counter+=1
+                print (i)
 
-            # this notifies when it's less than 30 min away
-            if counter>0:
-                for i in task_list:
-                    if i[3]==False:
-                        dateinfo = i[1].split('-')
-                        timeinfo = i[2].split(':')
-                        d1 = datetime.datetime(int(dateinfo[0]),int(dateinfo[1]),int(dateinfo[2]),int(timeinfo[0]),int(timeinfo[1]),int(timeinfo[2]))-datetime.timedelta(minutes=30)
-                        if now>=d1:
-                            notification = Notify()
-                            notification.title = f"Notification for Task: {i[0]}"
-                            notification.message = f"Scheduled for {d1}!"
-                            notification.send()
-                            c.execute(f"""UPDATE schedule
-                            SET notified = 1
-                            WHERE task="{i[0]}"
-                            """)
-                            connection.commit()
-                time.sleep(30)
-            else:
+        result = [[None]*10]
+
+        thread = Thread(target=Scheduler.notification_mode_input)
+        thread.start()
+
+        while True:
+            if result[0] == 'quit':
                 break
+            now = datetime.datetime.now()
+            for i in task_list:
+                dateinfo = i[1].split('-')
+                timeinfo = i[2].split(':')
+                d1 = datetime.datetime(int(dateinfo[0]),int(dateinfo[1]),int(dateinfo[2]),int(timeinfo[0]),int(timeinfo[1]),int(timeinfo[2]))-datetime.timedelta(minutes=30)
+                if now>=d1:
+                    notification = Notify()
+                    notification.title = f"Notification for Task: {i[0]}"
+                    notification.message = f"Scheduled for {d1}!"
+                    notification.audio = '/Users/marcuslai/Desktop/Codes/Python/Scheduler/notification_audio.wav'
+                    notification.icon = '/Users/marcuslai/Desktop/Codes/Python/Scheduler/icon.png'
+                    notification.send()
+                    c.execute(f"""UPDATE schedule
+                    SET notified = 1
+                    WHERE task="{i[0]}"
+                    """)
+                    connection.commit()
+                    print('''\n|Task Scheduler - Notification Mode| ("quit" to reenter Scheduling Mode)''')
+                    c.execute("""SELECT task,date,time,notified FROM schedule
+                    WHERE notified = 0
+                    """)
+                    task_list = c.fetchall()
+                    if len(task_list)==0:
+                        print('All tasks notified, "quit" to return...\n')
+                    else:
+                        print('|Unnotified tasks|')
+                        for i in task_list:
+                            print (i)
+
     else:
         print('\nCommand not recognized')
 
